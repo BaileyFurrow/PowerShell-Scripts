38f1dd4000bf562e8ba24dbb7b9d74222df2cab9
Bailey Furrow <baileyfurrow24@gmail.com>
2024-12-08 21:41:03 -0500

    removed unneeded files

--- test.ps1
+++
@@ -1,9 +0,0 @@
-
-       $date = Get-Date
-       $MonthName = $date.ToString("MMMM")
-       $first = Get-Date -Day 1
-
-
-       Write-Host "$MonthName $Year"
-       Write-Host "Su Mo Tu We Th Fr Sa"
-       $MyInvocation
